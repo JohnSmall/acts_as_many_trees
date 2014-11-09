859da731f08bcde8fa8c74188966aba8a0bc5d6d
John-Mainsys <john@faversham-mainsys>
2014-11-09 11:08:19 +0000

    added a simple generator

--- lib/generators/acts_as_many_trees/migration_generator.rb
+++ lib/generators/acts_as_many_trees/migration_generator.rb
@@ -8,22 +8,22 @@ module ActsAsManyTrees
       include ActiveRecord::Generators::Migration
 
       extend Forwardable
-      def_delegators :klass, :hierarchy_table_name
+      def_delegators :hierarchy_table_name
 
       def self.default_generator_root
         File.dirname(__FILE__)
       end
 
       def create_migration_file
-        migration_template 'create_hierarchies_table.rb.erb', "db/migrate/create_#{klass.hierarchy_table_name}.rb"
+        migration_template 'create_hierarchies_table.rb.erb', "db/migrate/create_#{hierarchy_table_name}.rb"
       end
 
       def migration_class_name
-        "Create#{klass.hierarchy_table_name.camelize}"
+        "Create#{hierarchy_table_name.camelize}"
       end
 
-      def klass
-         class_name.constantize
+      def hierarchy_table_name
+         (class_name+'Hierarchy').tableize
       end
     end
   end
