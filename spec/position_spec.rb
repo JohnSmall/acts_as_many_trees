e5de42eb241b5d730cbf56fccebc037d9120cfd7
John-Mainsys <john@faversham-mainsys>
2014-11-09 20:28:41 +0000

    started on putting between two roots

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 RSpec.describe 'order by position' do
    describe 'set the position in the hierarchy' do
      let(:items){create_list(:item,5)}
+     let(:lots_of_items){create_list(:item,10)}
      it 'should put a new record at the end' do
        items[4].parent=items[1]
        items[3].parent=items[1]
@@ -22,6 +23,9 @@ RSpec.describe 'order by position' do
        items[2].parent={new_parent:items[1],before_node:items[3]}
        items[0].parent={new_parent:items[1],before_node:items[4],after_node:items[3]}
        expect(items[1].children.pluck(:id)).to eq([2,3,0,4].map{|n| items[n].id})
+     end
+     it 'should put a last record between two roots' do
+
      end
      it 'should move all descendants when the parent moves'
      it 'should reset all position number when requested'
