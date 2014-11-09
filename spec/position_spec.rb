2406686009678c22bd08273f2b84fa3276e4ab06
John-Mainsys <john@faversham-mainsys>
2014-11-09 20:26:01 +0000

    got before, after and in between insertions working

--- spec/position_spec.rb
+++ spec/position_spec.rb
@@ -10,8 +10,19 @@ RSpec.describe 'order by position' do
        items[2].parent=items[1]
        expect(items[1].children.pluck(:id)).to eq([4,3,2].map{|n| items[n].id})
     end
-     it 'should allow a new record at be begining'
-     it 'should allow a new record between two siblings'
+     it 'should allow a new record at the begining' do
+       items[4].parent=items[1]
+       items[3].parent={new_parent:items[1],before_node:items[4]}
+       items[2].parent={new_parent:items[1],before_node:items[3]}
+       expect(items[1].children.pluck(:id)).to eq([2,3,4].map{|n| items[n].id})
+     end
+     it 'should allow a new record between two siblings' do
+       items[4].parent=items[1]
+       items[3].parent={new_parent:items[1],before_node:items[4]}
+       items[2].parent={new_parent:items[1],before_node:items[3]}
+       items[0].parent={new_parent:items[1],before_node:items[4],after_node:items[3]}
+       expect(items[1].children.pluck(:id)).to eq([2,3,0,4].map{|n| items[n].id})
+     end
      it 'should move all descendants when the parent moves'
      it 'should reset all position number when requested'
    end
